3db1cfb9a68372bfa8115a74552d1c8afd581549
cheandrey <skyandreymann@gmail.com>
2024-12-27 22:38:13 +0300

    LastCourseWork

--- config/settings.py
+++ config/settings.py
@@ -74,17 +74,31 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "lmsproject",
-        "USER" : "postgres",
-        "PASSWORD": 'marz2',
-        "HOST": "localhost",
-        "PORT": "5432",
-
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "lmsproject",
+#         "USER" : "postgres",
+#         "PASSWORD": 'marz2',
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
